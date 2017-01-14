1e46e23992ac15851fdff63a5c19b880a22b9b65
Takeru Ohta <phjgt308@gmail.com>
2017-01-14 17:08:29 +0900

    Rename: s/Weight/Capacity/

--- src/lib.rs
+++ src/lib.rs
@@ -5,55 +5,18 @@ use std::borrow::Borrow;
 use std::slice;
 use std::iter;
 
+
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
-pub struct Weight(f64);
-impl Weight {
-    pub fn new(weight: f64) -> Option<Self> {
-        if 0.0 < weight && weight <= 1.0 {
-            Some(Weight(weight))
-        } else {
+pub struct Capacity(u8);
+impl Capacity {
+    pub fn new(capacity: u8) -> Option<Self> {
+        if capacity == 0 {
             None
+        } else {
+            Some(Capacity(capacity))
         }
     }
-    pub fn as_f64(&self) -> f64 {
-        self.0
-    }
 }
-
-pub trait Point: PartialEq + Hash {
-    fn hash_with_item<T: Hash, H: Hasher>(&self, item: &T, hasher: &mut H) {
-        item.hash(hasher);
-        self.hash(hasher);
-    }
-    fn weight(&self) -> Weight {
-        Weight(1.0)
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct PlainPoint<P>(P);
-impl<P: PartialEq + Hash> PlainPoint<P> {
-    pub fn new(inner: P) -> Self {
-        PlainPoint(inner)
-    }
-    pub fn into_inner(self) -> P {
-        self.0
-    }
-}
-impl<P: PartialEq + Hash> Point for PlainPoint<P> {}
-
-impl Point for i8 {}
-impl Point for u8 {}
-impl Point for i16 {}
-impl Point for u16 {}
-impl Point for i32 {}
-impl Point for u32 {}
-impl Point for i64 {}
-impl Point for u64 {}
-impl<'a> Point for &'a str {}
-impl Point for String {}
-impl<'a> Point for &'a [u8] {}
-impl Point for Vec<u8> {}
 
 #[derive(Debug, Clone)]
 pub struct RendezvousPoints<P, H> {
@@ -61,7 +24,7 @@ pub struct RendezvousPoints<P, H> {
     hasher: H,
 }
 impl<P, H> RendezvousPoints<P, H>
-    where P: Point,
+    where P: PartialEq + Hash,
           H: BuildHasher
 {
     pub fn new(hasher: H) -> Self {
@@ -85,18 +48,77 @@ impl<P, H> RendezvousPoints<P, H>
             None
         }
     }
+    pub fn hasher(&self) -> H::Hasher {
+        self.hasher.build_hasher()
+    }
     pub fn calc_candidates<T: Hash>(&mut self, item: &T) -> Candidates<P> {
         let builder = &self.hasher;
         self.points.sort_by_key(|p| {
             let mut hasher = builder.build_hasher();
-            p.hash_with_item(item, &mut hasher);
-            ((hasher.finish() as f64) * p.weight().0) as u64
+            (item, p).hash(&mut hasher);
+            hasher.finish()
         });
         Candidates(self.points.iter().rev())
     }
 }
 impl<P> Default for RendezvousPoints<P, BuildHasherDefault<DefaultHasher>>
-    where P: Point
+    where P: PartialEq + Hash
+{
+    fn default() -> Self {
+        Self::new(BuildHasherDefault::default())
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct NonUniformRendezvousPoints<P, H> {
+    points: Vec<(P, Capacity)>,
+    hasher: H,
+}
+impl<P, H> NonUniformRendezvousPoints<P, H>
+    where P: PartialEq + Hash,
+          H: BuildHasher
+{
+    pub fn new(hasher: H) -> Self {
+        NonUniformRendezvousPoints {
+            points: Vec::new(),
+            hasher: hasher,
+        }
+    }
+    pub fn insert(&mut self, point: P, capacity: Capacity) -> Option<(P, Capacity)> {
+        let old = self.remove(&point);
+        self.points.push((point, capacity));
+        old
+    }
+    pub fn remove<Q>(&mut self, point: &Q) -> Option<(P, Capacity)>
+        where P: Borrow<Q>,
+              Q: PartialEq
+    {
+        if let Some(i) = self.points.iter().position(|p| p.0.borrow() == point) {
+            Some(self.points.swap_remove(i))
+        } else {
+            None
+        }
+    }
+    pub fn hasher(&self) -> H::Hasher {
+        self.hasher.build_hasher()
+    }
+    pub fn calc_candidates<T: Hash>(&mut self, item: &T) -> Candidates<(P, Capacity)> {
+        let builder = &self.hasher;
+        self.points.sort_by_key(|&(ref p, capacity)| {
+            (0..capacity.0)
+                .map(|i| {
+                    let mut hasher = builder.build_hasher();
+                    (item, p, i).hash(&mut hasher);
+                    hasher.finish()
+                })
+                .max()
+                .unwrap()
+        });
+        Candidates(self.points.iter().rev())
+    }
+}
+impl<P> Default for NonUniformRendezvousPoints<P, BuildHasherDefault<DefaultHasher>>
+    where P: PartialEq + Hash
 {
     fn default() -> Self {
         Self::new(BuildHasherDefault::default())
@@ -116,6 +138,7 @@ impl<'a, P: 'a> Iterator for Candidates<'a, P> {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
     use super::*;
 
     #[test]
@@ -124,6 +147,53 @@ mod tests {
         points.insert("foo");
         points.insert("bar");
         points.insert("baz");
-        assert_eq!(points.calc_candidates(&1).collect::<Vec<_>>(), [&"foo"]);
+        points.insert("qux");
+        assert_eq!(points.calc_candidates(&1).collect::<Vec<_>>(),
+                   [&"bar", &"baz", &"foo", &"qux"]);
+        assert_eq!(points.calc_candidates(&"key").collect::<Vec<_>>(),
+                   [&"qux", &"bar", &"foo", &"baz"]);
+
+        points.remove(&"baz");
+        assert_eq!(points.calc_candidates(&1).collect::<Vec<_>>(),
+                   [&"bar", &"foo", &"qux"]);
+        assert_eq!(points.calc_candidates(&"key").collect::<Vec<_>>(),
+                   [&"qux", &"bar", &"foo"]);
+
+        points.remove(&"bar");
+        assert_eq!(points.calc_candidates(&1).collect::<Vec<_>>(),
+                   [&"foo", &"qux"]);
+        assert_eq!(points.calc_candidates(&"key").collect::<Vec<_>>(),
+                   [&"qux", &"foo"]);
+
+        points.insert("bar");
+        points.insert("baz");
+        let mut counts = HashMap::new();
+        for item in 0..1000 {
+            let point = points.calc_candidates(&item).nth(0).unwrap();
+            *counts.entry(point.to_string()).or_insert(0) += 1;
+        }
+        assert_eq!(counts["foo"], 246);
+        assert_eq!(counts["bar"], 266);
+        assert_eq!(counts["baz"], 237);
+        assert_eq!(counts["qux"], 251);
+    }
+
+    #[test]
+    fn weighted_points() {
+        let mut points = NonUniformRendezvousPoints::default();
+        points.insert("foo", Capacity::new(70).unwrap());
+        points.insert("bar", Capacity::new(20).unwrap());
+        points.insert("baz", Capacity::new(9).unwrap());
+        points.insert("qux", Capacity::new(1).unwrap());
+
+        let mut counts = HashMap::new();
+        for item in 0..10000 {
+            let point = points.calc_candidates(&item).nth(0).unwrap();
+            *counts.entry(point.0.to_string()).or_insert(0) += 1;
+        }
+        assert_eq!(((counts["foo"] as f64) / 100.0).round(), 70.0);
+        assert_eq!(((counts["bar"] as f64) / 100.0).round(), 20.0);
+        assert_eq!(((counts["baz"] as f64) / 100.0).round(), 9.0);
+        assert_eq!(((counts["qux"] as f64) / 100.0).round(), 1.0);
     }
 }
