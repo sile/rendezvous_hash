820f4333f4028f8c1f11e4b0cae01d0a1b59df64
Takeru Ohta <phjgt308@gmail.com>
2017-01-17 00:33:06 +0900

    Improve benchmark command

--- examples/bench.rs
+++ examples/bench.rs
@@ -5,6 +5,7 @@ use std::collections::HashMap;
 use std::io::{BufRead, BufReader};
 use std::fs::File;
 use std::iter::FromIterator;
+use std::time::Instant;
 use clap::{App, Arg};
 use rendezvous_hash::RendezvousNodes;
 
@@ -20,17 +21,38 @@ fn main() {
             .min_values(1)
             .multiple(true))
         .get_matches();
+
     let filepath = matches.value_of("WORD_FILE").unwrap();
+    let words: Vec<_> = BufReader::new(File::open(filepath).expect("Cannot open file"))
+        .lines()
+        .collect::<Result<_, _>>()
+        .expect("Cannot read words");
+    println!("WORD COUNT: {}", words.len());
+
     let mut nodes = RendezvousNodes::default();
     nodes.extend(matches.values_of("NODES").unwrap());
+    println!("NODE COUNT: {}", nodes.len());
 
-    let mut selected_counts: HashMap<&str, _> =
-        HashMap::from_iter(matches.values_of("NODES").unwrap().map(|k| (k, 0)));
-    for line in BufReader::new(File::open(filepath).expect("Cannot open file")).lines() {
-        let line = line.expect("Cannot read a line");
-        let selected = nodes.calc_candidates(&line).nth(0).unwrap();
-        *selected_counts.get_mut(selected).unwrap() += 1;
+    let start_time = Instant::now();
+    for word in words.iter() {
+        nodes.calc_candidates(word).nth(0).unwrap();
+    }
+    let end_time = Instant::now();
+
+    let mut counts: HashMap<&str, _> = HashMap::from_iter(nodes.iter().map(|k| (*k, 0)));
+    for word in words.iter() {
+        let selected = nodes.calc_candidates(word).nth(0).unwrap();
+        *counts.get_mut(selected).unwrap() += 1;
     }
 
-    println!("SELECTED COUNT: {:?}", selected_counts);
+    println!("");
+    println!("SELECTED COUNT PER NODE:");
+    for (node, count) in counts {
+        println!("- {}: \t{}", node, count);
+    }
+    println!("");
+
+    let elapsed = end_time - start_time;
+    println!("ELAPSED: {} ms",
+             elapsed.as_secs() * 1000 + (elapsed.subsec_nanos() / 1_000_000) as u64);
 }
